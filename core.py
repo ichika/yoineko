10fc47007c746b78d54b0135909bb0c7c9440ba0
ichika <ichika@users.noreply.github.com>
2013-08-21 22:54:31 +0000

    xmpp

--- core.py
+++ core.py
@@ -23,8 +23,9 @@ class NodeBase:
 
 class Watashi(NodeBase):
     """my node"""
-    host = '127.0.0.1'
+    host = 'localhost'
     port = 2000
+    port_xmpp = 2012
 
     def __init__(self, port=None, db_name='data'):
         """run my node"""
@@ -40,7 +41,8 @@ class Watashi(NodeBase):
             print('exist', node, node.addr, node.port)
             self.call(node.addr, 'hello')
 
-        self.listen()
+        #self.listen()
+        self.listen_xmpp()
 
     def listen(self):
         """listen for events"""
@@ -49,6 +51,70 @@ class Watashi(NodeBase):
             response, addr = self.socket.recvfrom(1024)
             print('receive', response, addr)
             self.call(addr, 'asd', wait=False)
+
+    def listen_xmpp(self):
+        """listen for jabber connections"""
+        connection_data = b'''<?xml version='1.0'?>
+            <stream:stream xmlns:stream='http://etherx.jabber.org/streams'id='1'
+            xmlns='jabber:client' from='localhost'>'''
+
+        auth1_data = b'''<iq type='result' from='localhost' id='auth_1'>
+            <query xmlns='jabber:iq:auth'>
+                <username/>
+                <password/>
+                <resource/>
+            </query>
+        </iq>'''
+
+        auth2_data = b'''<iq type='result' from='localhost' id='auth_2'/>'''
+
+        roster_data = b'''<iq id='aab2a' type='result' from='localhost'>
+            <query xmlns='jabber:iq:roster'>
+                <item jid='sabine@yak' name='sabine' subscription='both'>
+                    <group>Family</group>
+                </item>
+            </query>
+        </iq>'''
+
+        list_data = b'''<iq id='aab3a' type='result'/><iq id='aab5a' type='result'/>'''
+
+        print('listen xmpp', self.host, self.port_xmpp)
+        self.socket_xmpp = socket.socket()
+        self.socket_xmpp.bind((self.host, self.port_xmpp))
+        self.socket_xmpp.listen(5)
+        connect, addr = self.socket_xmpp.accept()
+        print('connect xmpp', connect, addr)
+
+        # connection
+        data = connect.recv(1024)
+        print('receive', data)
+        connect.send(connection_data)
+        print('send   ', connection_data)
+
+        data = connect.recv(1024)
+        print('receive', data)
+        connect.send(auth1_data)
+        print('send   ', auth1_data)
+
+        data = connect.recv(1024)
+        print('receive', data)
+        connect.send(auth2_data)
+        print('send   ', auth2_data)
+
+        data = connect.recv(1024)
+        print('receive', data)
+        connect.send(roster_data)
+        print('send   ', roster_data)
+
+        data = connect.recv(1024)
+        print('receive', data)
+        connect.send(list_data)
+        print('send   ', list_data)
+
+        data = connect.recv(1024)
+        print('receive', data)
+        data = connect.recv(1024)
+        print('receive', data)
 
 
 class Node(NodeBase):
